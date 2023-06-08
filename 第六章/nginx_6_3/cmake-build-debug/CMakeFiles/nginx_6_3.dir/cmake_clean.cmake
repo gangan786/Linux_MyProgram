ae01a973f43bcadc002fd822c692f58f4ec9b16a
Gangan786 <215387011@qq.com>
2023-06-08 11:24:01 +0800

    6_3

---
+++ 第六章/nginx_6_3/cmake-build-debug/CMakeFiles/nginx_6_3.dir/cmake_clean.cmake
@@ -0,0 +1,28 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nginx_6_3.dir/app/nginx.cxx.o"
+  "CMakeFiles/nginx_6_3.dir/app/ngx_c_conf.cxx.o"
+  "CMakeFiles/nginx_6_3.dir/app/ngx_log.cxx.o"
+  "CMakeFiles/nginx_6_3.dir/app/ngx_printf.cxx.o"
+  "CMakeFiles/nginx_6_3.dir/app/ngx_setproctitle.cxx.o"
+  "CMakeFiles/nginx_6_3.dir/app/ngx_string.cxx.o"
+  "CMakeFiles/nginx_6_3.dir/logic/ngx_c_slogic.cxx.o"
+  "CMakeFiles/nginx_6_3.dir/misc/ngx_c_crc32.cxx.o"
+  "CMakeFiles/nginx_6_3.dir/misc/ngx_c_memory.cxx.o"
+  "CMakeFiles/nginx_6_3.dir/misc/ngx_c_threadpool.cxx.o"
+  "CMakeFiles/nginx_6_3.dir/net/ngx_c_socket.cxx.o"
+  "CMakeFiles/nginx_6_3.dir/net/ngx_c_socket_accept.cxx.o"
+  "CMakeFiles/nginx_6_3.dir/net/ngx_c_socket_conn.cxx.o"
+  "CMakeFiles/nginx_6_3.dir/net/ngx_c_socket_inet.cxx.o"
+  "CMakeFiles/nginx_6_3.dir/net/ngx_c_socket_request.cxx.o"
+  "CMakeFiles/nginx_6_3.dir/proc/ngx_daemon.cxx.o"
+  "CMakeFiles/nginx_6_3.dir/proc/ngx_event.cxx.o"
+  "CMakeFiles/nginx_6_3.dir/proc/ngx_process_cycle.cxx.o"
+  "CMakeFiles/nginx_6_3.dir/signal/ngx_signal.cxx.o"
+  "nginx_6_3"
+  "nginx_6_3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/nginx_6_3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
